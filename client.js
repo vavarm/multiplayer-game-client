2ab7c4bb896687c9f0b3b7c041d805d61430edb3
varm <v.racaud-minuzzi@orange.fr>
2022-03-11 19:52:06 +0100

    ➕ feat: client ables to connect to a server, create a room and send messages

--- client.js
+++ client.js
@@ -2,14 +2,29 @@ const WebSocket = require('ws')
 
 const connection = new WebSocket('ws://localhost:8080/')
 
-connection.onopen = () => {
+connection.on('open', () => {
     console.log('WebSocket Client Connected')
-}
-connection.onerror = error => {
+    createRoom()
+})
+connection.on('close', () => {
+    console.log("Connection lost")
+})
+connection.on('error', (error) => {
     console.log('Connection Error: ' + error.toString())
-}
-connection.onmessage = msg => {
-    console.log("Received: '", msg.data, "'")
+})
+connection.on('message', (data) => {
+    var dataJSON = JSON.parse(data)
+    console.log("Received: '", dataJSON.msg, "'")
+})
+
+function createRoom() {
+    if (connection.readyState == WebSocket.OPEN) {
+        var topic = "CreateRoom"
+        var roomName = "MyRoom"
+        var roomPwd = "MyPassword"
+        var msg = { 'topic': topic, 'name': roomName, 'pwd': roomPwd }
+        connection.send(JSON.stringify(msg))
+    }
 }
 
 function sendNumber() {
