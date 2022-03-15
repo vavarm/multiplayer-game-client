1482a58294e22b741b3893923818a6a2ba04ef2a
varm <v.racaud-minuzzi@orange.fr>
2022-03-15 12:50:34 +0100

    ➕ feat: client ables to join a room + choice entry method

--- client.js
+++ client.js
@@ -1,37 +1,70 @@
-const WebSocket = require('ws')
+const WebSocket = require("ws");
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-const connection = new WebSocket('ws://localhost:8080/')
+const connection = new WebSocket("ws://localhost:8080/");
 
-connection.on('open', () => {
-    console.log('WebSocket Client Connected')
-    createRoom()
-})
-connection.on('close', () => {
-    console.log("Connection lost")
-})
-connection.on('error', (error) => {
-    console.log('Connection Error: ' + error.toString())
-})
-connection.on('message', (data) => {
-    var dataJSON = JSON.parse(data)
-    console.log("Received: '", dataJSON.msg, "'")
-})
+connection.on("open", () => {
+    console.log("WebSocket Client Connected");
+    ChoiceEntry();
+});
+connection.on("close", () => {
+    console.log("Connection lost");
+});
+connection.on("error", (error) => {
+    console.log("Connection Error: " + error.toString());
+});
+connection.on("message", (data) => {
+    var dataJSON = JSON.parse(data);
+    console.log("Received: '", dataJSON.msg, "'");
+});
 
-function createRoom() {
+function CreateRoom(roomName, roomPwd) {
     if (connection.readyState == WebSocket.OPEN) {
-        var topic = "CreateRoom"
-        var roomName = "MyRoom"
-        var roomPwd = "MyPassword"
-        var msg = { 'topic': topic, 'name': roomName, 'pwd': roomPwd }
-        connection.send(JSON.stringify(msg))
+        var topic = "CreateRoom";
+        var msg = { topic: topic, name: roomName, pwd: roomPwd };
+        connection.send(JSON.stringify(msg));
     }
 }
 
+function JoinRoom(roomName, roomPwd) {
+    if (connection.readyState == WebSocket.OPEN) {
+        var topic = "JoinRoom";
+        var msg = { topic: topic, name: roomName, pwd: roomPwd };
+        connection.send(JSON.stringify(msg));
+    }
+}
+
+// TODO - entry method to let the client choose between create or join a room
+function ChoiceEntry() {
+    rl.question(
+        "Choose between create (1) or join (2) a room ",
+        function(choice) {
+            rl.question("Room name: ", function(roomName) {
+                rl.question("Password: ", function(roomPwd) {
+                    if (choice == 1) {
+                        CreateRoom(roomName, roomPwd);
+                    } else if (choice == 2) {
+                        JoinRoom(roomName, roomPwd);
+                    } else {
+                        console.error("Invalid choice");
+                    }
+                });
+            });
+        }
+    );
+}
+// TODO - clean this method
+/*
 function sendNumber() {
     if (connection.readyState == WebSocket.OPEN) {
-        var number = Math.round(Math.random() * 0xFFFFFF)
-        connection.send(number.toString())
+        var number = Math.round(Math.random() * 0xffffff); //Oxffffff is the color applied to the variable
+        connection.send(number.toString());
     }
-    setTimeout(sendNumber, 1000)
+    setTimeout(sendNumber, 1000);
 }
-sendNumber()
+sendNumber();
+*/
